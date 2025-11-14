64a7846a596c32a61ecec9d2e06bc0204d7f6d64
pn97arjun <pn97arjun@gmail.com>
2025-11-14 11:27:40 +0530

    encrypto file

---
+++ Utility/encrypto.js
@@ -0,0 +1,29 @@
+import CryptoJS from 'crypto-js';
+import dotenv from 'dotenv';
+
+
+export function decryptData(ciphertext, secretKey) {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
+function encryptFileJS() {
+    dotenv.config(); // Load environment variables from .env file
+
+    console.log("Encrypting password...");
+    const password = ""; // Example password to encrypt
+
+    // Retrieve the secret key from the environment variable
+    var secretKey = process.env.ENCRYPTION_KEY; 
+
+    if (!secretKey) {
+        console.error("Error: SECRET_KEY environment variable not set.");
+        return; // Exit if the secret key is not available
+    }
+
+    const encrypted = CryptoJS.AES.encrypt(password, secretKey).toString();
+    console.log("Encrypted Password:", encrypted);
+}
+
+encryptFileJS();
+
+
